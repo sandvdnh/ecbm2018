0278bdfc673c382f2055d5d845318fadbbf8b64d
lianeyoung <43224655+lianeyoung@users.noreply.github.com>
2018-12-13 18:12:18 -0500

    Working on training/validation

--- model.py
+++ model.py
@@ -7,13 +7,14 @@
 import time
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 from model_funcs import *
   
     
 class DCGAN:
     def __init__(self, sess, input_height=64, input_width=64, batch_size=64, sample_num = 64, output_height=64, output_width=64,
-                 g_dim=[1024, 512, 256, 128], d_dim=[64, 128, 256, 512], s_size=4, z_dim=100, dataset='SVHN'):
+                 g_dim=[1024, 512, 256, 128], d_dim=[64, 128, 256, 512], s_size=4, y_dim=None, z_dim=100, dataset='SVHN'):
         '''
         Initialize variables for implementation and calculations
         Args:
@@ -35,6 +36,7 @@ class DCGAN:
         self.d_dim = [3] + d_dim
 
         self.s_size = s_size
+        self.y_dim = y_dim
         self.z_dim = z_dim
         
         self.dataset = dataset
@@ -43,6 +45,7 @@ class DCGAN:
         
         
         self.z = tf.random_uniform([self.batch_size, self.z_dim], minval=-1.0, maxval=1.0)
+        self.z_sum = tf.summary.histogram("z", self.z)
         
         self.build_model()
         
@@ -143,28 +146,22 @@ class DCGAN:
         t_outputs = self.discriminator(inputs, training=True)
         
         # Softmax cross entropy loss
-        #g_loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=g_outputs, labels=tf.zeros([self.batch_size]))
-        g_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    labels=tf.ones([self.batch_size], dtype=tf.int64),
-                    logits=g_outputs)
+        #g_loss = tf.nn.sigmoid_cross_entropy_with_logits(logits=g_outputs, labels=tf.zeros([self.batch_size]))
+        g_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.ones([self.batch_size], dtype=tf.int64), logits=g_outputs)
         self.g_loss = tf.reduce_mean(g_loss)
         
-        #d_loss_real = tf.nn.softmax_cross_entropy_with_logits_v2(logits=t_outputs, labels=tf.ones([self.batch_size]))
-        d_loss_real = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    labels=tf.ones([self.batch_size], dtype=tf.int64),
-                    logits=t_outputs)
+        #d_loss_real = tf.nn.sigmoid_cross_entropy_with_logits(logits=t_outputs, labels=tf.ones([self.batch_size]))
+        d_loss_real = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.ones([self.batch_size], dtype=tf.int64), logits=t_outputs)
         self.d_loss_real = tf.reduce_mean(d_loss_real)
         
         #d_loss_fake = tf.nn.softmax_cross_entropy_with_logits_v2(logits=g_outputs, labels=tf.zeros([self.batch_size]))
-        d_loss_fake = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    labels=tf.zeros([self.batch_size], dtype=tf.int64),
-                    logits=g_outputs)
+        d_loss_fake = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.zeros([self.batch_size], dtype=tf.int64),logits=g_outputs)
         self.d_loss_fake = tf.reduce_mean(d_loss_fake)
         
         self.d_loss = self.d_loss_real + self.d_loss_fake
         
-    
-    def train(self, epochs=2, batch_size=245, learning_rate=0.0002, beta1=0.5):
+
+    def train(self, epochs=2, batch_size=245, learning_rate=0.0002, beta1=0.5, pre_trained_model=None):
         '''
         Input Args:
         batches - batches of images for training
@@ -178,90 +175,129 @@ class DCGAN:
         #update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         #with tf.control_dependencies(update_ops):
         
-        with tf.variable_scope('scope', reuse = tf.AUTO_REUSE ):
+        with tf.variable_scope('optim', reuse=tf.AUTO_REUSE):
             # using Adam optimizer as specified in the project paper
             d_optim = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1).minimize(self.d_loss) #generator optimizer
             g_optim = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1).minimize(self.g_loss) # discriminator optimizer
-
-        tf.global_variables_initializer().run()
+      
+        counter = 0
+        cur_model_name = 'DCGAN_{}'.format(int(time.time()))
         
-        counter = 1
+        tf.global_variables_initializer().run()
+        writer = tf.summary.FileWriter("log/{}".format(cur_model_name), self.sess.graph)
+        saver = tf.train.Saver()
+        
         start_time = time.time()
         
-        '''
-        # For loading a checkpoint
-        could_load, checkpoint_counter = self.load(self.checkpoint_dir)
-        if could_load:
-          counter = checkpoint_counter
-          print(" [*] Load SUCCESS")
-        else:
-          print(" [!] Load failed...")
-        '''
+        if pre_trained_model is not None:
+            try:
+                print("Load the model from: {}".format(pre_trained_model))
+                saver.restore(sess, 'model/{}'.format(pre_trained_model))
+            except Exception:
+                raise ValueError("Load model Failed!")
+                
+                
         # run
         #with tf.Session() as sess:
            # sess.run(init)
             
         #generate batches based on which dataset is being used
         #if self.data_set == '': --> uncomment if needed
-        print("loading data.... ")
         batches, iters = get_batches(batch_size, self.dataset)
-        
+                
         # getting number of training iterations
-        # iters = 1000
+        #iters = 1000
         print('number of batches for training: {}'.format(iters))
 
-        for epoch in range(epochs):
-            
+        for epc in range(epochs):
+            print("epoch {} ".format(epc))
             for i in range(iters):
+                counter += 1
                 #get batch
                 batch_images = next(batches)
 
-                batch_z = np.random.uniform(-1, 1, [config.batch_size, self.z_dim]).astype(np.float32)
+                batch_z = np.random.uniform(-1, 1, [self.batch_size, self.z_dim]).astype(np.float32)
 
                 #_, g_loss_value, d_loss_value = sess.run([train_op, losses[self.g], losses[self.d]])
 
                 # ---> Run g_optim twice to make sure that d_loss does not go to zero?
                 # Update D network
-                _, summary_str = self.sess.run([d_optim, self.d_sum], feed_dict={ self.inputs: batch_images, self.z: batch_z })
-                self.writer.add_summary(summary_str, counter)
+                _, D_loss_curr = self.sess.run([d_optim, self.d_loss], feed_dict={ self.inputs: batch_images, self.z: batch_z })
 
                 # Update G network
-                _, summary_str = self.sess.run([g_optim, self.g_sum], feed_dict={ self.z: batch_z })
-                self.writer.add_summary(summary_str, counter)
+                _, G_loss_curr = self.sess.run([g_optim, self.g_loss], feed_dict={ self.z: batch_z })
 
-                # ---> Run g_optim twice to make sure that d_loss does not go to zero (different from paper)?
-                '''
-                _, summary_str = self.sess.run([g_optim, self.g_sum], feed_dict={ self.z: batch_z })
-                self.writer.add_summary(summary_str, counter)
-                '''
-
-                errD_fake = self.d_loss_fake.eval({ self.z: batch_z })
-                errD_real = self.d_loss_real.eval({ self.inputs: batch_images })
+                # ---> Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
+                _, G_loss_curr = self.sess.run([g_optim, self.g_loss], feed_dict={ self.z: batch_z })
+                
+                errD_fake = self.d_loss_fake.eval({self.z: batch_z})
+                errD_real = self.d_loss_real.eval({self.inputs: batch_images})
                 errG = self.g_loss.eval({self.z: batch_z})
+                
+                if counter % 10 == 1:
+                    print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
+                      % (epc, epochs, i, iters,
+                        time.time() - start_time, errD_fake+errD_real, errG))
+                
+                if counter % 100 == 1:
+                    # do validation
+                    sample_in = tf.random_uniform([self.batch_size, self.z_dim], minval=-1.0, maxval=1.0)
+                    sample_input = self.sampler(sample_in)
+                    samples = self.sess.run([sample_input])
+                    #rescale generated image
+                    samples = (samples + 1) * 255 / 2
+                    #show generated image
+                    plt.imshow(samples)
+                    plt.show()
 
-                counter += 1
-                print("Epoch: [%2d/%2d] [%4d/%4d] time: %4.4f, d_loss: %.8f, g_loss: %.8f" \
-                  % (epoch, config.epoch, idx, batch_idxs,
-                    time.time() - start_time, errD_fake+errD_real, errG))
-
+                    plt.savefig('img_{}'.format(counter+epc))
                 '''
-                if np.mod(counter, 100) == 1:
                     try:
-                        samples, d_loss, g_loss = self.sess.run([self.sampler, self.d_loss, self.g_loss],
-                        feed_dict={
-                            self.z: sample_z,
-                            self.inputs: sample_inputs,
-                        },
-                        )
-                        save_images(samples, image_manifold_size(samples.shape[0]),
-                            './{}/train_{:02d}_{:04d}.png'.format(config.sample_dir, epoch, idx))
+                        sample_in = tf.random_uniform([self.batch_size, self.z_dim], minval=-1.0, maxval=1.0)
+                        sample_input = self.sampler(sample_in)
+                        samples = self.sess.run([sample_input])
+                        #rescale generated image
+                        samples = (samples + 1) * 255 / 2
+                        #show generated image
+                        plt.imshow(samples)
+                        plt.show()
+                        
+                        plt.savefig('img_{}'.format(counter+epc))
                         print("[Sample] d_loss: %.8f, g_loss: %.8f" % (d_loss, g_loss)) 
                     except:
                         print("one pic error!...")
-                '''
+                    '''
 
                 # Save checkpoint
-                if np.mod(counter, 500) == 2:
-                    self.save(config.checkpoint_dir, counter)
+                if counter % 500 == 1:
+                    saver.save(sess, 'model/{}'.format(cur_model_name))
+    
+    def sampler(self, z, y=None):
+        # This function creates sample images for validation in the training function
+        with tf.variable_scope('generator') as scope:
+            scope.reuse_variables()
+            
+            s_h, s_w = self.output_height, self.output_width
+            s_h2, s_w2 = conv_out_size_same(s_h, 2), conv_out_size_same(s_w, 2)
+            s_h4, s_w4 = conv_out_size_same(s_h2, 2), conv_out_size_same(s_w2, 2)
+            s_h8, s_w8 = conv_out_size_same(s_h4, 2), conv_out_size_same(s_w4, 2)
+            s_h16, s_w16 = conv_out_size_same(s_h8, 2), conv_out_size_same(s_w8, 2)
+
+            # project `z` and reshape
+            h0 = tf.reshape(linear(z, self.gf_dim*8*s_h16*s_w16, 'g_h0_lin'), [-1, s_h16, s_w16, self.gf_dim * 8])
+            h0 = tf.nn.relu(self.g_bn0(h0, train=False))
+
+            h1 = deconv2d(h0, [self.batch_size, s_h8, s_w8, self.gf_dim*4], name='g_h1')
+            h1 = tf.nn.relu(self.g_bn1(h1, train=False))
+
+            h2 = deconv2d(h1, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h2')
+            h2 = tf.nn.relu(self.g_bn2(h2, train=False))
+
+            h3 = deconv2d(h2, [self.batch_size, s_h2, s_w2, self.gf_dim*1], name='g_h3')
+            h3 = tf.nn.relu(self.g_bn3(h3, train=False))
+
+            h4 = deconv2d(h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4')
+
+            return tf.nn.tanh(h4)
 
         
