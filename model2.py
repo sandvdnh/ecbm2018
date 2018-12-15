6d8f8591ad42daae5385e24384e8b3137e3a248f
sandvdnh <sv2563@columbia.edu>
2018-12-14 22:55:43 -0500

    bla

--- model2.py
+++ model2.py
@@ -202,7 +202,9 @@ class DCGAN(object):
                 #print(batch_images[0])
                 #print(np.min(batch_images[0]))
                 #print(np.max(batch_images[0]))
-                batch_images = next(batches).astype(np.float32) / 255 * 2 - 1
+                for i in range(batch_idxs):
+                    print('yielding')
+                    batch_images = next(batches).astype(np.float32) / 255 * 2 - 1
                 #print(np.mean(batch_images[0]))
                 #print(batch_images[0])
                 #print(np.min(batch_images[0]))
