8c1f915db579717fee00f20c803ce23dfa6261e8
sandvdnh <sv2563@columbia.edu>
2018-12-15 20:12:36 -0500

    bla

--- model_funcs.py
+++ model_funcs.py
@@ -289,26 +289,24 @@ def binary_cross_entropy(preds, targets, name=None):
         return tf.reduce_mean(-(targets * tf.log(preds + eps) +
                               (1. - targets) * tf.log(1. - preds + eps)))
 
-#def conv_cond_concat(x, y):
-#    '''
-#    
-#    '''
-#    x_shapes = x.get_shape()
-#    y_shapes = y.get_shape()
-#    return tf.concat(3, [x, y*tf.ones([x_shapes[0], x_shapes[1], x_shapes[2], y_shapes[3]])])
-
-def conv2d(input_, output_dim,
-           k_h=5, k_w=5, d_h=2, d_w=2, stddev=0.02,
-           name="conv2d"):
+def conv2d(
+        input_,
+        output_dim,
+        height=5,
+        width=5,
+        height_=2,
+        width_=2,
+        stddev=0.02,
+        name="conv2d"):
     with tf.variable_scope(name):
-        w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim],
-                            initializer=tf.truncated_normal_initializer(stddev=stddev))
-        conv = tf.nn.conv2d(input_, w, strides=[1, d_h, d_w, 1], padding='SAME')
+        w = tf.get_variable(
+                'w',
+                [height, width, input_.get_shape()[-1], output_dim],
+                initializer=tf.truncated_normal_initializer(stddev=stddev))
 
+        conv = tf.nn.conv2d(input_, w, strides=[1, height_, width_, 1], padding='SAME')
         biases = tf.get_variable('biases', [output_dim], initializer=tf.constant_initializer(0.0))
-        # conv = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape())
         conv = tf.nn.bias_add(conv, biases)
-
         return conv
 
 def conv2d_transpose(input_, output_shape,
